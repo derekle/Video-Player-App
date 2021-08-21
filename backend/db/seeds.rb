f4fc7508cfd52cac2038e48868d30ee25ef51c71
derek <derekle.creative@gmail.com>
2021-08-21 18:05:25 -0500

    create seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Room.create(name: "123abc")
+Room.create(name: "xyz321")
+
+5.times do
+	u = []
+	u << User.create(name: "User_A", room_id: 1)
+end
+
+5.times do
+	u = []
+	u << User.create(name: "user_b", room_id: 2)
+end
+
+User.all.each do |x|
+	x.update(name: "#{x.name.downcase}_#{x.id}")
+	x.save
+end
