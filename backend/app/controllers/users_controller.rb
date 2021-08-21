e16b56004ec96619ba4af8293d4706dbf88aff38
derek <derekle.creative@gmail.com>
2021-08-21 18:04:25 -0500

    create CRUD functions for user & output to json

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users, except: [:created_at, :updated_at]
+  end
+
+  def create
+  end
+
+  def show
+    render json: get_user, except: [:created_at, :updated_at]
+  end
+
+  def destroy
+    get_user.destroy
+  end
+
+  private
+  def get_user
+    user = User.find_by(id: params[:id])
+  end
 end
