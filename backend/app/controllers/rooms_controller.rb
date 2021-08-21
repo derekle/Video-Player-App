b61ff332d605b3027e2743c525d4f73206b8bbdd
derek <derekle.creative@gmail.com>
2021-08-21 18:03:09 -0500

    add CRUD functions for rooms & output to json

--- backend/app/controllers/rooms_controller.rb
+++ backend/app/controllers/rooms_controller.rb
@@ -1,2 +1,22 @@
 class RoomsController < ApplicationController
+	def index
+		rooms = Room.all
+		render json: rooms, except: [:created_at, :updated_at]
+	end
+
+	def create
+	end
+
+	def show
+		render json: get_room, except: [:created_at, :updated_at]
+	end
+
+	def destroy
+		get_room.destroy
+	end
+
+	private
+	def get_room
+		 Room.find_by(id: params[:id])
+	end
 end
