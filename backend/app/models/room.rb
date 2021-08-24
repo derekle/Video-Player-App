7211981d23dd2151a41c0f499dbde07d0e473a6f
derek <derekle.creative@gmail.com>
2021-08-24 01:41:26 -0500

    fix syntax error

--- backend/app/models/room.rb
+++ backend/app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-    has_one: :playlist
-    has_many: :users
-    has_many: :songs, through: :playlist
+    has_one :playlist
+    has_many :users
+    has_many :songs, through: :playlist
 end
