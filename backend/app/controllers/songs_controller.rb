0001de88d8909ce1380ad948a78e8bad18e33071
derek <derekle.creative@gmail.com>
2021-08-28 03:19:45 -0500

    add crud actions for songs

--- backend/app/controllers/songs_controller.rb
+++ backend/app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+def index
+    render json: get_song, except: [:created_at, :updated_at]
+  end
+
+  def create
+    p params
+    s = Song.create!(source:params[:source], playlist_id:params[:playlist_id])
+    render json: s, except: [:created_at, :updated_at]
+  end
+
+  def show
+    render json: get_song, except: [:created_at, :updated_at]
+  end
+
+  def destroy
+    get_song.destroy
+  end
+
+  private
+  def get_song
+    song = Song.find_by(id: params[:id]) || User.all
+  end
 end
