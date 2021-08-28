230d2e9d285785ef960e020f096c4b90b8ed2c11
derek <derekle.creative@gmail.com>
2021-08-28 03:17:48 -0500

    made fetch functions only return data

--- frontend/fetch.js
+++ frontend/fetch.js
@@ -1,92 +1,64 @@
-async function fetchForm(ev, resrc) {
+function fillForm(ev, resrc, method) {
+	console.log(method);
+	//debugger;
 	// stop page from refreshing on submit
 	ev.preventDefault();
-
 	// specify which form to get information from (sender)
 	let myForm = ev.target;
-
 	// create form data object from submitted info (shipping label)
 	let fd = new FormData(myForm);
 	// convert form data to json (get right box size)
-	let json = await convert(fd);
+	let json = convert(fd);
 	convert(fd);
-
 	// get address to send package (recipient)
 	let url = 'http://localhost:3000/' + resrc;
-
 	// package information to send to server (box it up)
 	let req = new Request(url, {
 		body: json,
-		method: 'POST',
+		method: method,
 		headers: {
 			'Content-type': 'application/json; charset=UTF-8',
 		},
 	});
+	return req;
+}
 
-	// send package to server (shipped!)
-	fetch(req)
-		// receive response
-		.then((result) => result.json())
-		// throw out junk mail
-		.then((data) => {
-			if (resrc == 'rooms') {
-				addRow(
-					data.name,
-					resrc + 'Table',
-					resrc + 'Body',
-					data.users.length
-				);
-			}
-			if (resrc == 'users') {
-				session.username = data.name;
-				session.userID = data.id;
-				updateUserName();
-				deleteElementByID('usersForm');
-				renderRoom();
-			}
-		})
-		.catch((error) => {
-			console.error(error);
-		});
+function sendForm(req) {
+	return (
+		fetch(req)
+			// receive response
+			.then((result) => result.json())
+			// throw out junk mail
+			.then((data) => {
+				return data;
+			})
+	);
 }
 
 function fetchRoom() {
-	fetch('http://localhost:3000/rooms')
-		//deserialize js to return objects
-		.then((result) => result.json())
-		//extract data from array of objects
-		.then((data) => {
-			//build table out of data
-			for (obj of data) {
-				addRow(
-					obj.name,
-					'rooms',
-					'roomsTable',
-					'roomsBody',
-					obj.users.length
-				);
-			}
-		});
+	return (
+		fetch('http://localhost:3000/rooms')
+			//deserialize js to return objects
+			.then((result) => result.json())
+			//extract data from array of objects
+			.then((data) => {
+				//build table out of data
+				return data;
+			})
+	);
 }
 
 function fetchPlaylist(resrc) {
-	console.log(resrc);
-	fetch('http://localhost:3000/' + resrc)
-		//deserialize js to return objects
-		.then((result) => result.json())
-		//extract data from array of objects
-		.then((data) => {
-			console.log(data);
-			for (song of data.songs) {
-				addRow(
-					data.songs.indexOf(song),
-					'playlists',
-					'playlistsTable',
-					'playlistsBody',
-					song.source
-				);
-			}
-		});
+	return (
+		fetch('http://localhost:3000/' + resrc)
+			//deserialize js to return objects
+			.then((result) => result.json())
+			//extract data from array of objects
+			.then((data) => {
+				console.log(data);
+				return data;
+			})
+	);
 }
 
 function fetchRoomID() {
