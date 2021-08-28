6111575f01c0bd3779c819b4af599dd967d11b71
derek <derekle.creative@gmail.com>
2021-08-27 21:33:11 -0500

    add CRUD actions for playlists

--- backend/app/controllers/playlists_controller.rb
+++ backend/app/controllers/playlists_controller.rb
@@ -1,2 +1,25 @@
 class PlaylistsController < ApplicationController
+	def index
+		playlist = Playlist.all
+		render json: playlist, except: [:created_at, :updated_at], include: [:songs]
+	end
+
+	def create
+		p params[:name]
+		pl = Playlist.create!(room_id:params[:room_id])
+		render json: pl, except: [:created_at, :updated_at], include: [ :songs]
+	end
+
+	def show
+		render json: get_playlist, except: [:created_at, :updated_at], include: [:songs]
+	end
+
+	def destroy
+		get_playlist.destroy
+	end
+
+	private
+	def get_playlist
+		 Playlist.find_by(id: params[:id])
+	end
 end
