e4c1d563f03f4cec737f26580dc13cdf5c341ea2
derek <derekle.creative@gmail.com>
2021-08-29 16:16:13 -0500

    removed during development

--- backend/db/migrate/20210821004340_create_playlists.rb
+++
@@ -1,9 +0,0 @@
-class CreatePlaylists < ActiveRecord::Migration[6.1]
-  def change
-    create_table :playlists do |t|
-      t.belongs_to :room, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
