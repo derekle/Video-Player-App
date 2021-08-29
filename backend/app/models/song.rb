208590420fb99c6e518d3f0db513fab9ad5f7e57
derek <derekle.creative@gmail.com>
2021-08-29 17:51:29 -0500

    validate length

--- backend/app/models/song.rb
+++ backend/app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ApplicationRecord
   belongs_to :playlist
   validates :source, presence: true, uniqueness: true
+  validates_length_of :source, minimum: 43, maximum: 43, allow_blank: false
 end
