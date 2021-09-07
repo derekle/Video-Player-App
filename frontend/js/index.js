1a22db850aae0997dd103dde465c9cbcbc727c09
derek <derekle.creative@gmail.com>
2021-09-06 21:35:34 -0700

    updated to utilize new session object

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -14,8 +14,6 @@ async function buildroomList() {
 }
 
 function refreshBtn() {
-	debugger;
-
 	const refreshBtn = document.getElementById('refresh');
 	refreshBtn.addEventListener('click', async function () {
 		document.querySelectorAll('.currentRoom').forEach((a) =>
@@ -33,9 +31,11 @@ function formaddEventListener(resrc, method) {
 		'submit',
 		//when a user hits submit:
 		async function (event) {
+			debugger;
 			//[ 1 ] collect the data and package it into a request variable (JSON object)
 			let request = fillForm(event, resrc, method);
 			//[ 2 ] POST request to server, await for response
+			console.log(request);
 			data = await sendForm(request);
 			//[ 3 ] Manipulate the relevant DOM table for the resource, based on returned data
 			modifyTable(resrc, data);
@@ -45,27 +45,30 @@ function formaddEventListener(resrc, method) {
 
 // the function that is called when a user clicks on a room. assigned when building the table in addRow
 async function roomClicked(n, $this) {
+	console.log('roomClicked');
 	// reset the current song, since we're entering a new room
 	session.currentSong = null;
 	// remove any players that are on screen
 	deleteElementByID('player');
 	deleteElementByID('container');
-	console.log('roomClicked');
 	// set the local roomname to the argument that's been passed in (the name of the room)
-	session.roomname = n;
-
+	//session.roomname = n;
 	//Update DOM element - screen should now show the current room the user is in
-	updateDOMRoomName();
+	updateDOMRoomName(n);
 	// get the room id
-	const rid = await this.fetchRoomID();
-	session.roomID = rid;
-	setDOMCurrentRoom(rid, $this);
+	const currentRoom = await this.fetchRoomByName(n);
+	session.currentRoom = currentRoom;
+	//session.roomID = rid;
+	// highlight selected room by updating html class for css styling
+	setDOMCurrentRoom($this);
 	// get the playlist id
-	const pid = await this.fetchPlaylistID();
-	session.playlistID = pid;
-
+	const currentPlaylist = await this.fetchPlaylistByRoomID(
+		currentRoom.id
+	);
+	//session.playlistID = pid;
+	session.currentPlaylist = currentPlaylist;
 	// get the playlist
-	const plist = await fetchPlaylist('playlists/' + pid);
+	const plist = await fetchPlaylist(currentPlaylist.id);
 	setPlaylist(plist);
 
 	// if the room has an existing playlist with songs in it, set the first song to be the current song
